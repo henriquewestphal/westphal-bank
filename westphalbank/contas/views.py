416790d7f3c586c43a6967f07d39f87530540de6
henriquewestphal <henriquewalves@hotmail.com>
2018-02-09 17:07:34 -0200

    criação da tela login

--- westphalbank/contas/views.py
+++ westphalbank/contas/views.py
@@ -2,27 +2,32 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from contas.models import Conta, Convite
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def index(request):
     return render(request, 'index.html',{'contas': Conta.objects.all(), 'conta_logado': get_conta_logado(request)})
 
-
+@login_required
 def exibir(request, conta_id):
     conta = Conta.objects.get(id=conta_id)
     conta_logado = get_conta_logado(request)
     ja_e_contato = conta in conta_logado.contatos.all()
     return render(request, 'conta.html', {"conta" : conta, 'conta_logado': get_conta_logado(request), 'ja_e_contato': ja_e_contato})
 
+@login_required
 def convidar(request, conta_id):
     conta_a_convidar = Conta.objects.get(id=conta_id)
     conta_logado = get_conta_logado(request)
     conta_logado.convidar(conta_a_convidar)
     return redirect('index')
 
+@login_required
 def get_conta_logado(request):
-    return Conta.objects.get(id=1)
+    return request.user.conta
 
+@login_required
 def aceitar(request, convite_id):
     convite = Convite.objects.get(id=convite_id)
     convite.aceitar()
--- westphalbank/usuarios/urls.py
+++ westphalbank/usuarios/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from usuarios.views import RegistrarUsuarioView
+from usuarios import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^registrar/$', RegistrarUsuarioView.as_view(), name='registrar'),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/login/'}, name='logout'),
-
+    url(r'^login/$', auth_views.login, {'template_name':'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout_then_login, {'login_url':'/login/'}, name='logout'),
 ]
